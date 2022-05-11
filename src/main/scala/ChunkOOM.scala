bc9394b893897913921cd7a8e99dc6460255a13f
明扬 <my@famer.me>
2022-05-11 23:05:36 +0800

    BOOM!

--- src/main/scala/ChunkOOM.scala
+++ src/main/scala/ChunkOOM.scala
@@ -8,8 +8,7 @@ object ChunkOOM extends zio.App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
     ZStream.repeatEffect(zio.random.nextInt)
       .chunkN(128)
-      .as(Random.alphanumeric.take(1024 * 1024).mkString.getBytes("UTF-8"))
-      .mapConcat(bytes => bytes)
+      .mapConcat(_ => Random.alphanumeric.take(1024 * 1024).mkString.getBytes("UTF-8"))
       .chunkN(1024 * 10)
       .run(ZSink.foreachChunk(_ => ZIO.unit))
       .exitCode
