4da905fe7c29d316a88939d0fe6e5d12f8c4eee8
明扬 <my@famer.me>
2022-05-11 23:18:33 +0800

    BOOM!

--- src/main/scala/ChunkOOM.scala
+++ src/main/scala/ChunkOOM.scala
@@ -10,7 +10,7 @@ object ChunkOOM extends zio.App {
       .chunkN(128)
       .mapConcat(_ => Random.alphanumeric.take(1024 * 1024).mkString.getBytes("UTF-8"))
       .chunkN(1024 * 10)
-      .run(ZSink.foreachChunk(_ => ZIO.unit))
+      .runDrain
       .exitCode
   }
 }
