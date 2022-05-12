fa0f46e8cefde7793ce725cea47e2514d5cd042c
明扬 <my@famer.me>
2022-05-12 23:42:29 +0800

    BOOM!!!

--- src/main/scala/ChunkOOM.scala
+++ src/main/scala/ChunkOOM.scala
@@ -1,16 +1,10 @@
-import zio.stream.{ZSink, ZStream}
-import zio.{ExitCode, URIO, ZIO}
+import zio.Chunk
 
-import scala.util.Random
-
-object ChunkOOM extends zio.App {
-
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    ZStream.repeat()
-      .chunkN(128)
-      .mapConcat(_ => Random.alphanumeric.take(1024 * 1024).mkString.getBytes("UTF-8"))
-      .chunkN(1024 * 10)
-      .runDrain
-      .exitCode
+object ChunkOOM extends App {
+  var chunk = Chunk.fill(1025)(0.toByte)
+  while(true) {
+    chunk = chunk ++ Chunk.fill(1024)(0.toByte)
+    val (_, tail) = chunk.splitAt(1024)
+    chunk = tail
   }
 }
